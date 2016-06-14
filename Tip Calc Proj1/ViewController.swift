a0eb7732b7af1d19d8b433ade5150b8a39370326
Olivia Koshy <okoshy@fb.com>
2016-06-14 15:46:11 -0700

    New Tip Updates

--- Tip Calc Proj1/ViewController.swift
+++ Tip Calc Proj1/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var billField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 
+    @IBAction func calcTip(sender: AnyObject) {
+        let tipPercentages = [0.15, 0.18, 0.20]
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 
 }
 
