291c95d494af4de092667eefa5e077f55458de1b
Olivia Koshy <okoshy@fb.com>
2016-06-14 13:29:20 -0700

    Initial Commit

---
+++ Tip Calc Proj1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tip Calc Proj1
+//
+//  Created by Olivia Koshy on 6/14/16.
+//  Copyright © 2016 Olivia Koshy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
